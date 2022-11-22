2c542129404803e95168c558e14a5cbc165fe832
zc <2170943370@qq.com>
2022-11-22 14:53:55 +0800

    first commit

---
+++ src/main/java/com/zc/hello.java
@@ -0,0 +1,14 @@
+package com.zc;
+
+public class hello {
+    public static void main(String[] args) {
+        String s= """
+                hjxjsg
+                djfhjkd
+                """;
+        System.out.println(s);
+        int j=9;
+        System.out.println("hello world!!!");
+
+    }
+}
