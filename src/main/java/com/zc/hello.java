6373761ae8dba92538cf203f2ad00e942fe365df
Tom的冒险 <109362093+Take-risks@users.noreply.github.com>
2022-11-22 16:17:23 +0800

    pull test

--- src/main/java/com/zc/hello.java
+++ src/main/java/com/zc/hello.java
@@ -14,6 +14,7 @@ public class hello {
         System.out.println("master test");
         System.out.println("hot-fix test");
         System.out.println("push test222");
+        System.out.println("push test333");
 
     }
 }
